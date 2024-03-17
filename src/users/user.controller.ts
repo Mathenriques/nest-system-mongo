153bf6e60e28a777e01905f48f6373db5b9a56d8
Mathenriques <math.marqui@gmail.com>
2024-03-16 23:18:24 -0300

    Finalizando um projeto simples de implementação de mongodb com nestjs

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,6 +1,18 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { CreateUserDto } from "./users-dtos";
+import { UserService } from "./user.service";
 
 @Controller()
 export class UserController {
+  constructor(private readonly userService: UserService) {}
 
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Get()
+  async getAllUsers() {
+    return this.userService.findAll();
+  }
 }
